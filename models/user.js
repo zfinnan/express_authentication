1f78fe470d628dcd99e056d9063c5e44e0ce942e
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-11-23 11:53:34 -0500

    [user] Add model and validations

---
+++ models/user.js
@@ -0,0 +1,49 @@
+'use strict';
+const bcrypt = require('bcrypt') // making a hash password for the user
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class user extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  user.init({
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [1,99],
+          msg: 'Name must be between 1 and 99 characters'
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'Invalid email'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [12,99],
+          msg: 'Password must be between 12 and 99 characters'
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'user',
+  });
+  return user;
+};
